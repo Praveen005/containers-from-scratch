b2e66748e7cf4de620c26c624a3ef21afa3ad2b0
Praveen005 <praveen08h@gmail.com>
2024-06-28 13:41:01 +0530

    feat: set hostname for the newly created namespace. * First run is encountered. It calls run() * run() prepares the cmd to execute, * now, after cmd.Run(), a new namespace is created and then child() is called to set hostname for this namespace

--- Readme.md
+++ Readme.md
@@ -1,2 +1,25 @@
 
 code is from this [Talk](https://www.youtube.com/watch?v=8fi7uSYlOdc&ab_channel=GOTOConferences) from GoTo 2018
+
+
+## Namespaces:
+
+- Namespaces is where we limit what a process can see.
+- Created with syscalls
+- This is a big part of a container, it makes a container what it is, restricting the view the processes have of the host machine. 
+    - Unix Timesharing System 
+    - Process IDs 
+    - Mounts 
+    - Network 
+    - User IDs 
+    - InterProcess Comms
+
+
+## CGroups
+
+- What you can use 
+- Filesystem interface 
+    - Memory 
+    - CPU 
+    - I/O 
+    - Process numbers
--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
+// docker 			run image <cmd> <params>
+// go run main.go	run 	  <cmd> <params>
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 
 	default:
 		panic("bad command")
@@ -19,9 +24,32 @@ func main() {
 func run(){
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	// /proc/self/exe will ensure it run itself again
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// creating some namespaces, using system process attributes
+	// syscall.SysProcAttr is a struct that allows you to specify operating system-specific attributes for new processes. It is used in conjunction with the os/exec package to customize how processes are created and managed.
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// cloning is what creates a new process, we can arbitrary command in.
+		// we gonna start with flag for unix time sharing system.
+		// All there in UTS namespace is hostname. 
+		// But this is gonna let us have our own hostname inside the container, so can see its own and can't see what's happening on the host
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Run()
+}
+
+func child(){
+	fmt.Printf("Running child %v\n", os.Args[2:])
+	syscall.Sethostname([]byte("container"))
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
 }
+
+// To run: sudo go run main.go run /bin/bash
