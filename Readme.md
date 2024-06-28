182df257b287bc7bbabff2f2d2b22854afcfdd56
Praveen005 <praveen08h@gmail.com>
2024-06-28 18:59:25 +0530

    feat: add root directory for the container

--- Readme.md
+++ Readme.md
@@ -1,12 +1,21 @@
 
-code is from this [Talk](https://www.youtube.com/watch?v=8fi7uSYlOdc&ab_channel=GOTOConferences) from GoTo 2018
+Code is from this [Talk](https://www.youtube.com/watch?v=8fi7uSYlOdc&ab_channel=GOTOConferences) from GoTo 2018
 
 
 ## Namespaces:
 
+- It is a concept used in computing to create isolated environments.
+
+- Containers use namespaces to create isolated environments for running applications.
+
 - Namespaces is where we limit what a process can see.
+
 - Created with syscalls
+
 - This is a big part of a container, it makes a container what it is, restricting the view the processes have of the host machine. 
+
+Following are different types of namespaces:
+
     - Unix Timesharing System 
     - Process IDs 
     - Mounts 
@@ -23,3 +32,39 @@ code is from this [Talk](https://www.youtube.com/watch?v=8fi7uSYlOdc&ab_channel=
     - CPU 
     - I/O 
     - Process numbers
+
+
+
+## Working of container:
+
+1. Encapsulation into a container:
+
+The entity that encapsulates these namespaces into what we call a "container" is not a single technological component, but rather the container runtime (like Docker). The runtime creates and manages these namespaces together, along with other isolation features like cgroups.
+
+2. Functioning inside a container:
+
+While each namespace isolates a particular aspect of the system, they work together within the container to provide a complete, isolated environment. Here's how:
+
+- `UTS namespace`: Isolates hostname and domain name
+
+- `PID namespace`: Provides an isolated process tree
+
+- `Network namespace`: Isolates network interfaces, routing tables, etc.
+
+- `Mount namespace`: Provides an isolated file system view
+
+- `IPC namespace`: Isolates inter-process communication resources
+
+- `User namespace`: Isolates user and group ID number spaces
+
+These namespaces aren't completely separate; they interact with each other within the confines of the container. 
+
+3. Container Creation Process:
+
+    When a container is created:
+
+    - The container runtime creates new instances of each required namespace
+
+    - It then launches the container's init process (e.g., your application) within these namespaces
+
+    - This init process becomes PID 1 in the container's `PID namespace`
--- main.go
+++ main.go
@@ -12,8 +12,30 @@ import (
 
 // we still don't have namespace process id.
 // if you run: sudo go run main.go run /bin/bash and then run `ps`, it will show you large numbered PIDs
+/*
+PID TTY          TIME CMD
+88447 pts/14   00:00:00 sudo
+88448 pts/14   00:00:00 go
+88567 pts/14   00:00:00 main
+88573 pts/14   00:00:00 exe
+88579 pts/14   00:00:00 bash
+88635 pts/14   00:00:00 ps
+*/
 // we want the PIDs for the process inside the container to start from 1
 // we can do it by using syscall.CLONE_NEWPID while creating the namespace
+// Try running the command above again, and see
+/*
+  PID TTY          TIME CMD
+98911 pts/14   00:00:00 sudo
+98912 pts/14   00:00:00 go
+99017 pts/14   00:00:00 main
+99022 pts/14   00:00:00 exe
+99027 pts/14   00:00:00 bash
+99079 pts/14   00:00:00 ps
+*/
+
+// It still doesn't work, beacuse `ps` don't get the PIDs magically, but gets from the /proc directory, check by ls /proc
+// so, inside my container it needs its own /proc directory, currently it is seeing the same /proc directory
 func main() {
 	switch os.Args[1] {
 	case "run":
@@ -41,7 +63,26 @@ func run(){
 
 func child(){
 	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
 	syscall.Sethostname([]byte("container"))
+	// A container should have its own root file system.
+	// This is a fundamental principle of containerization, which ensures that containers are isolated from each other and the host system.
+	// container uses as its root directory. It contains all the files and directories necessary for the container's processes to run.
+	// Lizz used Vagarant-fs, we will use ubuntu file system.
+	// way to do:
+	// start the ubuntu container, extract its file system in a tar file, store it, make a root directory for your container, extract the fs here, and use it as the container's file system
+	/*
+
+	docker run -d --rm --name ubuntufs ubuntu:20.04 sleep 1000
+	docker export ubuntufs -o ubuntufs.tar
+	docker stop ubuntufs
+	sudo mkdir -p /container-root
+	sudo tar xf ubuntufs.tar -C /container-root/
+
+
+	*/
+	syscall.Chroot("/container-root")
+	syscall.Chdir("/")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -49,3 +90,10 @@ func child(){
 	cmd.Stderr = os.Stderr
 	cmd.Run()
 }
+
+// Start the container:  sudo go run main.go run /bin/bash
+// In the container, start a process:  sleep 100   
+// Go to another terminal outside the container i.e. from host: ps -C sleep
+// get the PID(put in place of 55327), and run `sudo ls -l /proc/55327/root`
+// you will get something like: lrwxrwxrwx 1 root root 0 Jun 28 18:48 /proc/55327/root -> /container-root
+// you can see /container-root is the root directory of the running container
